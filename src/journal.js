24bfa1f6dd5175d5af9fe4fd37980e78f45887cd
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-13 16:27:02 -0700

    finish front end logic in main.js

--- src/journal.js
+++ src/journal.js
@@ -1,19 +1,39 @@
-export function Entry(title,body) {
-this.title= title;
-this.body=body;
-this.wordCount=1;
-this.vowelWords=0;
-this.constWords=0;
+export function Entry(title, body) {
+  this.title = title;
+  this.body = body;
+  this.wordCount = 1;
+  this.vowelWords = 0;
+  this.constWords = 0;
 }
-Entry.prototype.vowelWords = function (){
-  var arrayBody = this.body.split ("");
+Entry.prototype.vowelWords = function () {
+  var arrayBody = this.body.split("");
   var vowels = 0;
-  var wordCount= 0;
+  var wordCount = 0;
   var constWords = 0;
-  arrayBody.forEach(function(letter){
-    for (letter === 'a') || letter=== 'e' || letter=== 'i' ||  letter === 'o' || letter === 'u') {
-      
+  arrayBody.forEach(function (letter) {
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u" || letter === "y") {
+      vowels += 1;
+    vowels += 1;
+  } else if (letter === '') {
+    wordCount += 1
+  } else if (letter === "." || letter === "," || letter === "!" || letter === "?") {
+    wordCount += 0
+  } else{
+    constWords +=1;
+
   }
+
+});
+
+this.vowelWords= vowels;
+this.wordCount += wordCount;
+this.constCount = constWords;
+return Entry
+};
+Entry.prototype.getTeaser= function() {
+  var arrayWord = this.body.split("",8);
+  var teaser= arrayWord.join("") +"...";
+  console.log(teaser)
 }
 
 
@@ -21,20 +41,5 @@ Entry.prototype.vowelWords = function (){
 
 
 
-  
 
 
-var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
-    }
-  }
-  return output;
-}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-impirt 
+import { Entry } from './journal';
+import '.styles.css';
+
+$(document).ready (function(){
+  $('.journalInput').submit(function(event){
+    event.preventDefult();
+    var inputBody = $("#journal-body").val();
+    var inputTitle = $("#journal-title").val();
+ var entryNew = new Entry (inputTitle,inputBody);
+ entryNew.vowelWords();
+ entryNew.getTeaser();
+ $(".outputEntry").text("There are" + entryNew.vowelWords + "vowels in your sentence." + "There are" + entryNew.wordCount + "words in your sentence.")
+
+  });
+});
