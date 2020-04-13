d41a4b84404358d6ae9398f6a3b2d2cb8a51a839
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-13 15:41:51 -0700

    add to journal.js page

--- src/journal.js
+++ src/journal.js
@@ -1,16 +1,30 @@
 export function Entry(title,body) {
 this.title= title;
 this.body=body;
-this.wordCount=0;
+this.wordCount=1;
+this.vowelWords=0;
+this.constWords=0;
+}
+Entry.prototype.vowelWords = function (){
+  var arrayBody = this.body.split ("");
+  var vowels = 0;
+  var wordCount= 0;
+  var constWords = 0;
+  arrayBody.forEach(function(letter){
+    for (letter === 'a') || letter=== 'e' || letter=== 'i' ||  letter === 'o' || letter === 'u') {
+      
+  }
+}
 
 
 
 
 
 
+  
 
 
-  var output = [];
+var output = [];
   for (var i = 1; i <= goal; i++) {
     if (i % 15 === 0) {
       output.push("ping-pong");
